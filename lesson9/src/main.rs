ee3137f8feef9374f894c55e246e9dbacc186dd4
td <td@gmail.com>
2024-04-17 17:05:15 +0200

    valtozo hazza adva

--- lesson9/src/main.rs
+++ lesson9/src/main.rs
@@ -1,3 +1,4 @@
 fn main() {
     println!("Hello, world!");
+    let mut a: &str = "Hello, world!";
 }
--- lesson9/target/debug/deps/lesson9-4c11cf4858ec9d7f.d
+++ lesson9/target/debug/deps/lesson9-4c11cf4858ec9d7f.d
@@ -1,5 +1,5 @@
-D:\Prog\Rust\lesson9\target\debug\deps\liblesson9-4c11cf4858ec9d7f.rmeta: src\main.rs
+D:\Prog\Rust\Lesson8_\lesson9\target\debug\deps\liblesson9-4c11cf4858ec9d7f.rmeta: src\main.rs
 
-D:\Prog\Rust\lesson9\target\debug\deps\lesson9-4c11cf4858ec9d7f.d: src\main.rs
+D:\Prog\Rust\Lesson8_\lesson9\target\debug\deps\lesson9-4c11cf4858ec9d7f.d: src\main.rs
 
 src\main.rs:
